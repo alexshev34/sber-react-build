29904d77ed5ec95383c010cc19f2dd3c63eb51a2
Alex <alexshevtsov2013@gmail.com>
2020-12-17 18:39:30 +0500

    Updates

---
+++ precache-manifest.374219364f3179fd348854efc5121145.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059c55a2212e83447d24fce1b7a49ad3",
+    "url": "/sber-react-build/index.html"
+  },
+  {
+    "revision": "fa379857d7e7ec3f51cb",
+    "url": "/sber-react-build/static/css/main.80a3eb76.chunk.css"
+  },
+  {
+    "revision": "6224cf45897eba4e6f32",
+    "url": "/sber-react-build/static/js/2.b6e22310.chunk.js"
+  },
+  {
+    "revision": "7b2d499757f074408b675bf81ca3e694",
+    "url": "/sber-react-build/static/js/2.b6e22310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa379857d7e7ec3f51cb",
+    "url": "/sber-react-build/static/js/main.d14c8180.chunk.js"
+  },
+  {
+    "revision": "a16c38474b09f909c29e",
+    "url": "/sber-react-build/static/js/runtime-main.0b8644a4.js"
+  },
+  {
+    "revision": "4116676aa84047d2734aa50a6497f850",
+    "url": "/sber-react-build/static/media/MyriadPro-Bold.4116676a.woff"
+  },
+  {
+    "revision": "7306ca385faec3b685ce39843c8c5f7c",
+    "url": "/sber-react-build/static/media/MyriadPro-Bold.7306ca38.woff2"
+  },
+  {
+    "revision": "279813ef49683eb8fcf0fc8258432406",
+    "url": "/sber-react-build/static/media/MyriadPro-Regular.279813ef.woff2"
+  },
+  {
+    "revision": "290905e4d206286fbaf729f7f3d4c565",
+    "url": "/sber-react-build/static/media/MyriadPro-Regular.290905e4.woff"
+  }
+]);
